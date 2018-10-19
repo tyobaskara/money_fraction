d17adcbad0192fad5bfa072bc08f14d5af1f63af
tyobaskara <prasetyaajibaskara@gmail.com>
2018-10-19 20:47:34 +0700

    deploy

--- server/server.js
+++ server/server.js
@@ -5,10 +5,10 @@ const app = express();
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(__dirname, 'client/dist'));
+  app.use(express.static(__dirname, 'client', 'dist'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/dist/index.html'));
+    res.sendFile(path.resolve(__dirname, 'client','dist','index.html'));
   });
 }
 
