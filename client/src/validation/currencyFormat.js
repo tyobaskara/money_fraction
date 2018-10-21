b8981b5b085fd8eebc23caff4be69f3b8a63240f
tyobaskara <prasetyaajibaskara@gmail.com>
2018-10-21 17:53:24 +0700

    update regex currency format

--- client/src/validation/currencyFormat.js
+++ client/src/validation/currencyFormat.js
@@ -1,5 +1,5 @@
 const currencyFormat  = string => {;
-    return string.match( /^(([a-zA-Z][0-9])|([a-zA-Z]+[\s]{0,1}[0-9])|([0-9.]))*[,0-9]{0,3}$/gm);
+    return string.match( /^(([a-zA-Z][\d])|([a-zA-Z]+[\s]{0,1}[\d])|(([\d]{1,3})*[.]*[\d]{3}))*[,\d]{0,3}$/gm);
 }
 
 export { currencyFormat };
